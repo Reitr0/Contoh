218c9139a7990176c4399c071922f88394f09fc7
unknown <m.rafi3344@gmail.com>
2021-01-29 17:36:17 +0700

    Pet Java

---
+++ MyPetTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author World Explorer
+ */
+public class MyPetTest {
+    
+    public MyPetTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        System.out.println("Start");
+    }
+    
+    @After
+    public void tearDown() {
+        System.out.println("Stop");
+    }
+
+//    @Test
+//    public void testSomeMethod() {
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+    
+    
+}
